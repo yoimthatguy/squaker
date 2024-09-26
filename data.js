99fa433a8207bc8b03cdd33b175606e09bca3ed4
Drew <esdproductions@gmail.com>
2024-09-26 00:32:37 -0400

    Initial Commit

---
+++ data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@NASA ✅`,
+        profilePic: `nasa.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a mission to Mars. No experience necessary`,
+        replies: [
+                  {
+                handle: `@WilliamShatner ✅`,
+                profilePic: `shatner.jpg`,
+                tweetText: `Yes! Where do I sign me up!`,
+            },
+                  {
+                handle: `@ElonMusk ✅`,
+                profilePic: `elonmusk.jpg`,
+                tweetText: `I went last year.`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+
+    {
+        handle: `@FunnyGuy15`,
+        profilePic: `funnyguy.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `If giants arent real then explain the 12 foot skeletons at Home Depot`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+        {
+        handle: `@FozzyTheClown`,
+        profilePic: `clown.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Whenever I am worried I messed up with my wife I remember that time my brother gave an anniversary card to his current wife on the date of his first marriage`,
+        replies: [
+            {
+                handle: `@JoeyFromBk`,
+                profilePic: `overflow.png`,
+                tweetText: `It's like the guy who has been married only once and is STILL married to her, but introduces her as his first wife.`,
+            },
+            {
+                handle: `@Passenger57`,
+                profilePic: `lady.jpg`,
+                tweetText: `Was he buried or cremated?`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
